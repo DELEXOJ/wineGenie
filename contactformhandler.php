40fd523f584e6ad576b4880bedef6b0c91630736
Ayobami Esther Ojo <36674978+DELEXOJ@users.noreply.github.com>
2020-07-06 06:41:53 -0400

    Add files via upload

--- contactformhandler.php
+++ contactformhandler.php
@@ -22,10 +22,24 @@ if (isset($_POST['submit'])) {
  $vistor_email=$_POST['email'];
  $message=$_POST['messsage'];
 
- $mailTo="winegenie@winegenie.ca";
- $header="From: $vistor_email\r\n";
- $txt="You have a new message from  ".$name.".\n\n\n".$message;
- mail($mailTo,$txt,$header);
+
+ require_once('PHPMailer/PHPMailerAutoload.php');
+ $mail=new PHPMailer();
+ $mail->isSMTP();
+ $mail->SMTPAuth()=true;
+ $mail->SMTPSecure='ssl';
+ $mail->Host='smtp.gmail.com';
+ $mail->Port='465';
+ $mail->isHTML();
+ $mail->Username='winegenie@winegenie.ca'
+ $mail->Password='wi2ne?genie4';
+ $mail->Body="You have a new message from  ".$name.".\n\n\n".$message;
+ $mail->AddAddress($vistor_email);
+ $mail->Send();
+ // $mailTo="winegenie@winegenie.ca";
+ // $header="From: $vistor_email\r\n";
+ // $txt="You have a new message from  ".$name.".\n\n\n".$message;
+ // mail($mailTo,$txt,$header);
             header("Location: contact.html?mailsent");
 }
 ?>
