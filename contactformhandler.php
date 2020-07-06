4c78ca83a37e6598a85c562af5aaebf5de76e0ba
Ayobami Esther Ojo <36674978+DELEXOJ@users.noreply.github.com>
2020-07-06 06:06:56 -0400

    Add files via upload

--- contactformhandler.php
+++ contactformhandler.php
@@ -1,20 +1,31 @@
 <?php
-$name=$_POST['name'];
-$vistor_email=$_POST['email'];
-$message=$_POST['messsage'];
+// $name=$_POST['name'];
+// $vistor_email=$_POST['email'];
+// $message=$_POST['messsage'];
+//
+// $email_from='ayobamiesther212@gmail.com';
+// $email_subject="New Feedback Submission";
+// $email_body="User Name: $name\n".
+//             "User Email: $visitor_email.\n\n\n".
+//             "User Message:\n $message.\n";
+//
+//
+//             $to="ayobamiesther212@gmail.com";
+//             $header="From: $email_from\r\n";
+//             $header="Reply-To: $visitor_email\r\n";
+//             mail($to,$email_subject,$email_body,$header);
+//             header("Location: contact.html");
 
-$email_from='ayobamiesther212@gmail.com';
-$email_subject="New Feedback Submission";
-$email_body="User Name: $name\n".
-            "User Email: $visitor_email.\n\n\n".
-            "User Message:\n $message.\n";
+if (isset($_POST['submit'])) {
+  // code...
+  $name=$_POST['name'];
+ $vistor_email=$_POST['email'];
+ $message=$_POST['messsage'];
 
-
-            $to="ayobamiesther212@gmail.com";
-            $header="From: $email_from\r\n";
-            $header="Reply-To: $visitor_email\r\n";
-            mail($to,$email_subject,$email_body,$header);
-            header("Location: contact.html");
-
-
+ $mailTo="winegenie@winegenie.ca";
+ $header="From: $vistor_email\r\n";
+ $txt="You have a new message from  ".$name.".\n\n\n".$message;
+ mail($mailTo,$txt,$header);
+            header("Location: contact.html?mailsent");
+}
 ?>
